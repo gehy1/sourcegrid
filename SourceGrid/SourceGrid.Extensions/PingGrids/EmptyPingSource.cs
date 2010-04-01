0aedadaf24e371ac042f8129068d43f75f770124
dariusdamalakas <dariusdamalakas@localhost>
2010-04-01 17:01:02 +0300

    Adding DataSet backend

---
+++ SourceGrid/SourceGrid.Extensions/PingGrids/EmptyPingSource.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace SourceGrid.Extensions.PingGrids
+{
+	public class EmptyPingSource : IPingData
+	{
+		public object GetItemValue(int index, string propertyName)
+		{
+			throw new NotImplementedException();
+		}
+		
+		public int Count {
+			get {
+			return 0; 
+			}
+			set {
+			}
+		}
+		
+		public bool AllowSort {
+			get {
+				return false;
+			}
+			set {
+			}
+		}
+		
+		public void ApplySort(string propertyName, bool @ascending)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
