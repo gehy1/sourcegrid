512fb9ea86fb0a9aa00ad442ac2addd5bf042113
Darius Damalakas <darius.damalakas@gmail.com>
2010-04-12 11:34:42 +0300

    Automaticlly copying fbembed.dll to bin directory

---
+++ SourceGrid.Examples/ServiceFactory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Castle.Windsor;
+
+namespace WindowsFormsSample
+{
+	public class ServiceFactory
+	{
+		private static WindsorContainer m_container = null;
+		
+		public static WindsorContainer Container {
+			get { return m_container; }
+			set { m_container = value; }
+		}
+		
+		public static T GetService<T>()
+		{
+			return m_container.Resolve<T>();
+		}
+		
+	}
+}
