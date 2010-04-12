2d58fdc19987368a331d63af8ef509843e301dd9
Darius Damalakas <darius.damalakas@gmail.com>
2010-04-12 11:06:22 +0300

    Adding NH backend

---
+++ SourceGrid.Examples/GridSamples/frmSample60.cs
@@ -0,0 +1,131 @@
+﻿
+using System;
+using System.Windows.Forms;
+using FluentNHibernate.Cfg;
+using Microsoft.Isam.Esent.Collections.Generic;
+using NHibernate;
+using NHibernate.Cfg;
+using NHibernate.Tool.hbm2ddl;
+using SourceGrid.PingGrid.Backend.Essent;
+using SourceGrid.PingGrid.Backend.NHibernate;
+
+namespace WindowsFormsSample.GridSamples.PingGrids
+{
+	[Sample("SourceGrid - PingGrid", 60, "PingGrid - NHibernate backend with FireBird")]
+	public partial class frmSample60 : Form
+	{
+		string[] customerNames = {"FireBird", "MySQL", "PostGre", "DivanDB", "CouchDB"};
+		Random rnd = new Random();
+		ISessionFactory sessionFactory = null;
+		NHibernatePingData<Track> source = null;
+			
+		private ISessionFactory CreateSessionFactory()
+		{
+			var conf = new FluentNHibernate.Cfg.Db.FirebirdConfiguration()
+				.ShowSql()
+				.ConnectionString(@"
+User=SYSDBA;Password=masterkey;Database=..\..\CHINOOK.FDB;
+DataSource=localhost; Port=3050;Dialect=3; Charset=UNICODE_FSS;Role=;Connection lifetime=15;Pooling=true;
+MinPoolSize=0;MaxPoolSize=50;Packet Size=8192;ServerType=1;");
+			
+			return Fluently.Configure()
+				.Database(conf)
+				.Mappings(m =>
+				          m.FluentMappings.AddFromAssemblyOf<frmSample60>())
+				.ExposeConfiguration(BuildSchema)
+				.BuildSessionFactory();
+		}
+		
+		private static void BuildSchema(Configuration config)
+		{
+			// delete the existing db on each run
+			//if (File.Exists(DbFile))
+			//    File.Delete(DbFile);
+
+			// this NHibernate tool takes a configuration (with mapping info in)
+			// and exports a database schema from it
+			new SchemaExport(config)
+				.Create(false, false);
+		}
+		
+		public frmSample60()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+			
+			sessionFactory = CreateSessionFactory();
+			source = new NHibernatePingData<Track>(sessionFactory);
+			
+			
+			
+			pingGrid1.Columns.Add("Composer", "Composer property", typeof(string));
+			pingGrid1.Columns.Add("Name", "Name property", typeof(string));
+			
+			
+			pingGrid1.DataSource = source;
+			pingGrid1.Columns.StretchToFit();
+			
+			pingGrid1.VScrollPositionChanged += delegate { UpdateCount(); };
+			toolStripMenuItem1.Click += this.ToolStripMenuItem1Click;
+			
+			AddRows(1, 10000);
+			UpdateCount();
+		}
+
+		void UpdateCount()
+		{
+			UpdateCountInternal(pingGrid1.DataSource.Count);
+		}
+		
+		void UpdateCountInternal(int count)
+		{
+			if (pingGrid1.Rows.FirstVisibleScrollableRow == null)
+				return;
+			int row = pingGrid1.Rows.FirstVisibleScrollableRow.Value;
+			this.toolStripStatusLabel1.Text = string.Format("Viewing record {0}/ {1}", row, pingGrid1.DataSource.Count);
+		}
+
+		private void AddRows(int from, int to)
+		{
+			var count = pingGrid1.DataSource.Count;
+			if (count > to )
+				return;
+			using (var session = sessionFactory.OpenSession())
+			{
+				using (var transaction = session.BeginTransaction())
+				{
+					for (var i = from; i < to; i++)
+					{
+						// create a couple of Stores each with some Products and Employees
+						var track = new Track{ Name = "SuperMart", Composer = "random composer" };
+						session.Save(track);
+						
+						if (i % 100 == 0)
+						{
+							session.Flush();
+							session.Clear();
+						}
+					}
+					transaction.Commit();
+
+				}
+			}
+			source.Invalidate();
+		}
+		
+		void ToolStripMenuItem1Click(object sender, EventArgs e)
+		{
+			var max = pingGrid1.DataSource.Count + 1;
+			AddRows(max, max + 200000);
+			UpdateCount();
+		}
+	}
+	
+
+}
---
+++ SourceGrid.PingGrid.Backend.NHibernate/NHibernatePingData.cs
@@ -0,0 +1,100 @@
+﻿
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+using NHibernate;
+using NHibernate.Criterion;
+using NHibernate.Transform;
+using SourceGrid.Extensions.PingGrids;
+
+namespace SourceGrid.PingGrid.Backend.NHibernate
+{
+	public class NHibernatePingData<T> : IPingData where T : class
+	{
+		ISessionFactory sessionFactory = null;
+		
+		/// <summary>
+		/// Use this to inject some custom performance optimized
+		/// property value reader.
+		/// </summary>
+		public IPropertyResolver PropertyResolver{get;set;}
+		
+		private int? count = null;
+		
+		public void Invalidate()
+		{
+			count = null;
+		}
+		
+		public NHibernatePingData(ISessionFactory factory)
+		{
+			this.sessionFactory = factory;
+			PropertyResolver = ReflectionPropertyResolver.SharedInstance;
+		}
+		
+		
+		public int Count {
+			get {
+				if (count == null)
+				{
+					using (var session = sessionFactory.OpenSession())
+					{
+						// populate the database
+						using (var transaction = session.BeginTransaction())
+						{
+							var criteria = session.CreateCriteria(typeof(T))
+								.SetProjection(Projections.RowCount());
+							var res = criteria.UniqueResult<int>();
+							count = res;
+						}
+					}
+				}
+				return count.Value;
+			}
+		}
+		
+		public bool AllowSort {
+			get {
+				throw new NotImplementedException("allowSourt");
+			}
+			set {
+				throw new NotImplementedException("allowSourt");
+			}
+		}
+		
+		public void ApplySort(string propertyName, bool @ascending)
+		{
+			throw new NotImplementedException("sort");
+		}
+		
+		
+		/// <summary>
+		/// Gets value from NHibernate.
+		/// 
+		/// In fact we do not need whole object here, w
+		/// </summary>
+		/// <param name="index"></param>
+		/// <param name="propertyName"></param>
+		/// <returns></returns>
+		public object GetItemValue(int index, string propertyName)
+		{
+			using (var session = sessionFactory.OpenStatelessSession())
+			{
+				// populate the database
+				using (var transaction = session.BeginTransaction())
+				{
+					var criteria = session.CreateCriteria(typeof(T))
+						.Add(Restrictions.IdEq(index));
+					var obj = criteria.UniqueResult();
+					transaction.Commit();
+					if (obj == null)
+						return null;
+					var value = PropertyResolver.ReadValue(obj, propertyName);
+					return value;
+				}
+			}
+		}
+		
+	}
+}
